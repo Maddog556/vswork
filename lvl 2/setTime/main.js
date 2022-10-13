8499ce6c59d5724c07073e519ae664b2aa7665f6
josh maddox <maddoxjosh310@yahoo.com>
2022-10-13 13:22:39 -0400

    localStorage and intervals here

---
+++ lvl 2/setTime/main.js
@@ -0,0 +1,38 @@
+
+
+
+const btn = document.getElementById("click")
+let counter = document.getElementById("output")
+let tim = document.getElementById("time")
+let lastNum = document.getElementById("record")
+
+// setting the numbers values 
+let numberOfClicks = 0
+output.value=numberOfClicks
+// saveing to localStorage
+// let storage = localStorage.setItem(output,numberOfClicks);
+  
+
+//when btn is click adds 1 to the counter
+btn.addEventListener("click", event =>{
+    event.preventDefault()
+    output.value = numberOfClicks +=1;
+localStorage.setItem(output,numberOfClicks);
+})
+
+let count = parseInt(localStorage.getItem(output,numberOfClicks));
+lastNum.append(count)
+
+// the countdown
+ let timer = 10
+let stopClock = setInterval(function(){
+    if(timer === 0){
+        clearInterval(stopClock);
+        alert("Time is Up")
+    }else;{
+        document.getElementById("time").innerHTML = timer +" seconds remaining"
+        timer-=1; 
+    }
+},1000);
+setTimeout(stopClock,10000);
+
