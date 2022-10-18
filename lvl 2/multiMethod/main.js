cbb9dc7350b53b3efd154631b4bf2e38369b61fb
josh maddox <maddoxjosh310@yahoo.com>
2022-10-18 12:44:24 -0400

    good examples

---
+++ lvl 2/multiMethod/main.js
@@ -0,0 +1,72 @@
+
+
+
+
+//1. Returns a list of everyone older than 18, which is
+//2. sorted alphabetically by last name, and where
+//3. each name and age is embedded in a string that looks like an 
+//HTML `<li>` element.
+
+let peopleArray = [
+    {
+        firstName: "Sarah",
+        lastName: "Palin",
+        age: 47
+    },
+    {
+        firstName: "Frank",
+        lastName: "Zappa",
+        age: 12
+    },
+    {
+        firstName: "Rick",
+        lastName: "Sanchez",
+        age: 78
+    },
+    {
+        firstName: "Morty",
+        lastName: "Smith",
+        age: 29
+    },
+    {
+        firstName: "Kyle",
+        lastName: "Mooney",
+        age: 27
+    },
+    {
+        firstName: "Pasha",
+        lastName: "Datsyuk",
+        age: 13
+    },
+    {
+        firstName: "Lev",
+        lastName: "Tolstoy",
+        age: 82
+    }
+]
+function sortedOfAge(arr){
+    return arr.filter(a => a.age >= 18)
+     .sort((a,b) => {
+        if(a.lastName < b.lastName){
+            return -1;
+        }if (a.lastName > b.lastName){
+            return 1;
+        }
+        
+    })
+    .map(people => `<li> ${people.lastName}  ${people.firstName} is  ${people.age} </li>`) 
+ }
+ 
+ console.log(sortedOfAge(peopleArray));
+ 
+ /*
+ Output:
+ [
+     "<li>Kyle Mooney is 27</li>",
+     "<li>Sarah Palin is 47</li>",
+     "<li>Rick Sanchez is 78</li>",
+     "<li>Morty Smith is 29</li>",
+     "<li>Lev Tolstoy is 82</li>"
+ ]
+ */
+ 
